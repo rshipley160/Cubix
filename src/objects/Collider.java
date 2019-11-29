1a2378c60dc1693d7f87215d34e20b4306b2046e
Riley Shipley <rshipley160@gmail.com>
2019-11-29 16:21:38 -0500

    Added exits

---
+++ src/objects/Collider.java
@@ -0,0 +1,42 @@
+package objects;
+
+import edu.utc.game.GameObject;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.Rectangle;
+
+public class Collider extends GameObject {
+    protected Rectangle shape;
+
+    public Collider(Rectangle shape, Rectangle collider)
+    {
+        this.shape = shape;
+        this.hitbox = collider;
+    }
+
+    public void setCollider(Rectangle collider) {
+        this.hitbox = collider;
+    }
+
+    public Rectangle getCollider() {
+        return hitbox;
+    }
+
+    @Override
+    public void draw() {
+        GL11.glColor3f(r,g,b);
+
+        float x=(float)shape.x;
+        float y=(float)shape.y;
+        float width=(float)shape.width;
+        float height=(float)shape.height;
+
+
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glVertex2f(x, y);
+        GL11.glVertex2f(x+width, y);
+        GL11.glVertex2f(x+width, y+height);
+        GL11.glVertex2f(x, y+height);
+        GL11.glEnd();
+    }
+}
--- src/objects/Exit.java
+++ src/objects/Exit.java
@@ -5,16 +5,26 @@ import edu.utc.game.GameObject;
 import edu.utc.game.Texture;
 import org.lwjgl.opengl.GL11;
 
-public class Exit extends GameObject {
+import java.awt.*;
+
+public class Exit extends Collider {
     private int cellConversion = 32;
     private Texture texture = new Texture("res\\exits.png");
     private Player.COLORS color = Player.COLORS.RED;
-
+    private static int ID = 1;
+    private String tag;
 
     public Exit (int x, int y, Player.COLORS color)
     {
-        this.hitbox.setBounds(Game.ui.getWidth()/2+x*cellConversion,Game.ui.getHeight()/2+y*cellConversion, 128, 64);
+        super(
+                new Rectangle(Game.ui.getWidth()/2+x*32,Game.ui.getHeight()/2+y*32, 128, 64),
+                new Rectangle(Game.ui.getWidth()/2+x*32,Game.ui.getHeight()/2+(y+1)*32, 128, 32)
+        );
         this.color = color;
+    }
+
+    public Player.COLORS getColor() {
+        return color;
     }
 
     @Override
@@ -27,13 +37,13 @@ public class Exit extends GameObject {
         texture.bind();
         GL11.glBegin(GL11.GL_QUADS);
         GL11.glTexCoord2f(0f + adjust,0);
-        GL11.glVertex2f(this.hitbox.x, this.hitbox.y);
+        GL11.glVertex2f(this.shape.x, this.shape.y);
         GL11.glTexCoord2f(0.5f + adjust,0);
-        GL11.glVertex2f(this.hitbox.x+this.hitbox.width, this.hitbox.y);
+        GL11.glVertex2f(this.shape.x+this.shape.width, this.shape.y);
         GL11.glTexCoord2f(0.5f + adjust,1);
-        GL11.glVertex2f(this.hitbox.x+this.hitbox.width, this.hitbox.y+this.hitbox.height);
+        GL11.glVertex2f(this.shape.x+this.shape.width, this.shape.y+this.shape.height);
         GL11.glTexCoord2f(0f + adjust,1);
-        GL11.glVertex2f(this.hitbox.x, this.hitbox.y+this.hitbox.height);
+        GL11.glVertex2f(this.shape.x, this.shape.y+this.shape.height);
         GL11.glEnd();
 
         GL11.glBindTexture(GL11.GL_TEXTURE_2D,  0);
